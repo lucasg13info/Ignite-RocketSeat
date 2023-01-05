1f50151e356e65710e2a073a7274cd2e20f6db57
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-01-05 00:06:58 -0300

    Estilizando com styled components

--- Ignite-timer/src/components/Button.styles.ts
+++ Ignite-timer/src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -19,6 +19,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     height: 40px
 
     ${props => {
-        return `background-color: ${buttonVariants[props.variant]}` 
+        return css`background-color: ${buttonVariants[props.variant]}` 
     }}
 `
