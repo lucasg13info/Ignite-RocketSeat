6e2330d272be358a36f50638b0f2367ff6b37605
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-01-08 23:41:12 -0300

    Estilos globais com Style-cmoponents

--- Ignite-timer/src/components/Button.styles.ts
+++ Ignite-timer/src/components/Button.styles.ts
@@ -22,6 +22,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     margin: 8px;
 
     background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
 
     /* ${props => {
         return css`background-color: ${buttonVariants[props.variant]}` 
